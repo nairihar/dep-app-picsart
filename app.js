1a631129054eb73934c62d2c9e00961662cf85b4
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2021-04-08 20:12:25 +0400

    Change port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+console.log(port);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
