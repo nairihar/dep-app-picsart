91608c0cb546572d21aad6105d44e545f04e3f42
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2021-04-13 18:47:33 +0400

    Change log

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
 
 
